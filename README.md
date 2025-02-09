496e96e3ecbab88749b6da97fca8e50f0145dc47
vikram007007 <alexandervikram80@gmail.com>
2025-02-09 21:55:37 +0530

    completed mini redis server

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CodingChallenges_redis
 # Commands to RUN
+.\venv\Scripts\activate -> to activate venv
 python .\src\main\mini_redis.py -> to start the mini server
 <br>docker run --name redis-server -p 6379:6379 -d redis -> to start redis-server tools on docker
 <br>docker run --rm redis redis-benchmark -t set,get -n 100000 -q -h host.docker.internal -p 6379 -> to run benchmark
-<br>docker run -it --rm redis redis-cli -h host.docker.internal -p 637 -> to use redis-cli
+<br>docker run -it --rm redis redis-cli -h host.docker.internal -p 6379 -> to use redis-cli
--- src/main/mini_redis.py
+++ src/main/mini_redis.py
@@ -1,13 +1,50 @@
 import asyncio
+import re
+import sys
+import os
+import json
 from typing import Any
 from resp_codec import RESPCodec, SimpleString
 
 class MiniRedisServer:
 
+    BASE_DIR = os.path.join(os.path.dirname(__file__), "resources", "cache_loader")
+    DATA_FILE = os.path.join(BASE_DIR, "kvstore.json")
+
     def __init__(self, host: str = "0.0.0.0", port: int = 6379) -> None:
         self.host = host
         self.port = port
-        self.store: dict[str, str] = {}
+        self.store: dict[str, Any] = {}
+        self.ensure_directory_exists()
+        self.load_data()
+
+    def ensure_directory_exists(self):
+        if not os.path.exists(self.BASE_DIR):
+            os.makedirs(self.BASE_DIR, exist_ok=True)
+            print(f"Directories created : {self.BASE_DIR}")
+
+    def save_data(self):
+        try:
+            with open(self.DATA_FILE, "w") as f:
+                json.dump(self.store, f, indent=4)
+            print(f"Data saved successfully to disk at {self.DATA_FILE}")
+            return SimpleString("OK")
+        except Exception as e:
+            print(f"ERR saving data : {e}")
+            return Exception("ERR saving data to disk")
+
+    def load_data(self):
+        if os.path.exists(self.DATA_FILE):
+            try:
+                with open(self.DATA_FILE, "r") as f:
+                    self.store = json.load(f)
+                print(f"Data loaded successfully from disk at {self.DATA_FILE}")
+            except Exception as e:
+                print(f"ERR loading data from disk : {e}")
+                self.store = {}
+
+    def is_valid_integer(self, s) -> bool:
+        return bool(re.fullmatch(r"-?\d+", s))
 
     async def handle_client(self, reader : asyncio.StreamReader, writer : asyncio.StreamWriter) -> None:
         addr = writer.get_extra_info("peername")
@@ -50,7 +87,10 @@ class MiniRedisServer:
                     else:
                         key = command[1]
                         value = command[2]
-                        self.store[key] = value
+                        if self.is_valid_integer(value):
+                            self.store[key] = int(value)
+                        else:
+                            self.store[key] = value
                         response = SimpleString("OK")
 
                 elif cmd == "GET":
@@ -64,18 +104,109 @@ class MiniRedisServer:
                     if len(command) < 2:
                         response = Exception("ERR wrong number of arguments for 'DEL'")
                     else:
-                        key = command[1]
-                        if key in self.store:
-                            del self.store[key]
-                            response = 1
-                        else:
-                            response = 0
+                        deleted_keys = 0
+                        for i in range(1, len(command)):
+                            key = command[1]
+                            if key in self.store:
+                                self.store.pop(key)
+                                deleted_keys += 1
+                        response = deleted_keys
 
                 elif cmd == "EXIT":
                     response = SimpleString("Bye")
                     writer.write(RESPCodec.encode(response))
                     await writer.drain()
                     break
+
+                elif cmd == "EXISTS":
+                    if len(command) < 2:
+                        response = Exception("ERR wrong number of arguments for 'EXISTS'")
+                    else:
+                        present_keys = 0
+                        for i in range(1, len(command)):
+                            key = command[i]
+                            if key in self.store:
+                                present_keys += 1
+                        response = present_keys
+
+                elif cmd == "INCR":
+                    if len(command) < 2:
+                        response = Exception("ERR wrong number of arguments for 'INCR'")
+                    else:
+                        key = command[1]
+                        success_flag = True
+                        if key in self.store:
+                            if isinstance(self.store[key], int) and (self.store[key] + 1) != sys.maxsize:
+                                self.store[key] += 1
+                            else:
+                                print(f"ERR value type is not an integer or out of range : {self.store[key]} {type(self.store[key])}")
+                                response = Exception("ERR value type is not an integer or out of range")
+                                success_flag = False
+                        else:
+                            self.store[key] = 1
+
+                        if success_flag:
+                            response = self.store[key]
+
+                elif cmd == "DECR":
+                    if len(command) < 2:
+                        response = Exception("ERR wrong number of arguments for 'DECR'")
+                    else:
+                        key = command[1]
+                        success_flag = True
+                        if key in self.store:
+                            if isinstance(self.store[key], int) and (self.store[key] - 1 != (-sys.maxsize - 1)):
+                                self.store[key] -= 1
+                            else:
+                                print(f"ERR value type is not an integer or out of range : {self.store[key]} {type(self.store[key])}")
+                                response = Exception("ERR value type is not an integer or out of range")
+                                success_flag = False
+                        else:
+                            self.store[key] = -1
+
+                        if success_flag:
+                            response = self.store[key]
+
+                elif cmd == "LPUSH":
+                    if len(command) < 3:
+                        response = Exception("ERR wrong number of arguments for 'LPUSH'")
+                    else:
+                        success_flag = True
+                        key = command[1]
+                        head_list = []
+                        if key in self.store:
+                            if isinstance(self.store[key], list):
+                                head_list = list(self.store[key])
+                            else:
+                                response = Exception("WRONGTYPE Operation against a key holding the wrong kind of value")
+                                success_flag = False
+                        if success_flag:
+                            for i in range(2, len(command)):
+                                head_list.insert(0, command[i])
+                            self.store[key] = head_list
+                            response = len(head_list)
+
+                elif cmd == "RPUSH":
+                    if len(command) < 3:
+                        response = Exception("ERR wrong number of arguments for 'RPUSH'")
+                    else:
+                        success_flag = True
+                        key = command[1]
+                        head_list = []
+                        if key in self.store:
+                            if isinstance(self.store[key], list):
+                                head_list = list(self.store[key])
+                            else:
+                                response = Exception("WRONGTYPE Operation against a key holding the wrong kind of value")
+                                success_flag = False
+                        if success_flag:
+                            for i in range(2, len(command)):
+                                head_list.append(command[i])
+                            self.store[key] = head_list
+                            response = len(head_list)
+
+                elif cmd == "SAVE":
+                    response = self.save_data()
 
                 else:
                     response = Exception(f"ERR Unknown command '{cmd}'")
